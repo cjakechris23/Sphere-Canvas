c2e4f3c6889372dfb032369c56a15a2a3d847b63
Jake Chris Delos Reyes <39973112+cjakechris23@users.noreply.github.com>
2024-05-15 09:25:34 +0800

    texture Update

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 import * as THREE from 'three'
+import gsap from 'gsap';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 
+const textureLoad = new THREE.TextureLoader()
+const myTexture = textureLoad.load("/mallows.jpg")
+
 const geometry = new THREE.SphereGeometry( 3, 64, 64);
-const material = new THREE.MeshStandardMaterial( { color: '#00ff83', roughness:0.5 } );
+//color: '#00ff83',
+const material = new THREE.MeshBasicMaterial( {  roughness:0.5 , map: myTexture} );
 const mesh = new THREE.Mesh( geometry, material );
 scene.add(mesh);
 
@@ -67,9 +72,30 @@ const loop = () => {
 }
 loop()
 
+const tl = gsap.timeline({defaults: {duration:1}})
+tl.fromTo(mesh.scale,{z:0,x:0,y:0},{z:1,x:1,y:1})
+tl.fromTo('nav',{y:'-100%'},{y:"0%"})
+tl.fromTo(".title",{opacity:0},{opacity:1})
+
 let mouseDown = false
 let rgb = []
 window.addEventListener("mousedown", () => (mouseDown = true))
 window.addEventListener("mouseup", () => (mouseDown = false))
 
-
+window.addEventListener("mousemove", (e) => {
+    if(mouseDown){
+        rgb = [
+            Math.round((e.pageX/sizes.width) * 255),
+            Math.round((e.pageY/sizes.height) * 255),
+            150,
+        ]
+        console.log("Test Color " + rgb)
+        let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+        gsap.to(mesh.material.color,{
+            r: newColor.r,
+            g: newColor.g,
+            b: newColor.b,
+        })
+       
+    }
+})
